f0f5ac1f7d4edbb5ea9ca052331ee5dc5e232b7d
ignatova-nata <ignatova_nata@inbox.ru>
2019-10-25 23:29:03 +0300

    JS L1

---
+++ L1/js/js.js
@@ -0,0 +1,16 @@
+
+var number = 450;
+otvet = number%2 === 0 ? '- число четное'  : '- число нечетное';
+//console.log(number, otvet);
+
+var m = 11.45;
+var n = 8.5;
+x = Math.abs(10 - m) < Math.abs(10 - n) ? m  : n;
+console.log(x);
+
+var h = 1;
+var a = 2;
+var b = 3;
+s = a * b;
+//console.log(s);
+console.log(h > b ? "высота больше ширины" : "ширина больше высоты");
